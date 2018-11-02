6c06b9df84ae0e1fc023c63e9e42d2fe4b4fcfe3
mbag24 <40617051+michaelbaggett@users.noreply.github.com>
2018-11-01 22:26:24 -0500

    page shows question as well as choice array answers, there may be a better way to do this

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,33 +2,45 @@
 //need array of multiple questions/answers
 var question1 = {
     text: "question text",
-    choices: ["choice1", "choice2", "choice3"],
+    choices: ["top", "bottom", "flex"],
     answer: "whichever choice array.index is correct answer"
 }
 var question2 = {
     text: "question text",
-    choices: ["choice1", "chocie2", "choice3"],
+    choices: ["blondes", "brunettes", "redheads"],
     answer: "whichever choice array index is correct"
 }
-var questoin3 = {
+
+var question3 = {
     text: "question text",
     choices: ["choice1", "chocie2", "choice3"],
     answer: "whichever choice array index is correct"
 }
-displayQuestion = function(){
+
+displayQuestion = function() {
     console.log(question1.text)
 
 }
 
 
-    
+
 ;
 
 $("#question").text(question1.text)
-question1.choices.forEach(element => {$("#choices").text(element)
-    
+    // forEach is the fucking worst
+question1.choices.forEach((choice, index) => {
+    console.log(choice, index)
+
+    var choiceClass = choice + index.toString()
+    $("#choices").append("<li class='" + choiceClass + "' value=" + choice + ">" + choice + "</li>")
+
+    $(`.${choiceClass}`).on('click', function(event) {
+        console.log(event)
+        console.log('MUTHA FUCKIN VALUE', event.target.value)
+        console.log('click')
+    })
 });
-    
+
 
 // $(".btn-lrg").on("click", function(){
 //     displayQuestion()
