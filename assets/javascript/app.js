39bb315048804b129bc846567757f04fa6e2a993
mbag24 <40617051+michaelbaggett@users.noreply.github.com>
2018-11-02 09:40:00 -0500

    put quiz questions into array instead of separate variables, created display question fuction that runs on click to play button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,46 +1,55 @@
 //pseudo code for trivia game
 //need array of multiple questions/answers
-var question1 = {
+var quizQuestion = [    
+    {
     text: "question text",
     choices: ["top", "bottom", "flex"],
-    answer: "whichever choice array.index is correct answer"
-}
-var question2 = {
+    answer: "top"
+},
+    {
     text: "question text",
-    choices: ["blondes", "brunettes", "redheads"],
+    choices: ["wrong", "correct", "close but no cigar"],
     answer: "whichever choice array index is correct"
-}
+},
 
-var question3 = {
+    {
     text: "question text",
     choices: ["choice1", "chocie2", "choice3"],
     answer: "whichever choice array index is correct"
 }
+]
 
-displayQuestion = function() {
-    console.log(question1.text)
 
-}
+//displayQuestion();
+
+i = 0
+
 
 
 
 ;
-
-$("#question").text(question1.text)
+var displayQuestion = function(){
+$("#question").text(quizQuestion[i].text)
     // forEach is the fucking worst
-question1.choices.forEach((choice, index) => {
+quizQuestion[i].choices.forEach((choice, index) => {
     console.log(choice, index)
 
     var choiceClass = choice + index.toString()
     $("#choices").append("<li class='" + choiceClass + "' value=" + choice + ">" + choice + "</li>")
 
     $(`.${choiceClass}`).on('click', function(event) {
-        console.log(event)
-        console.log('MUTHA FUCKIN VALUE', event.target.value)
+        console.log(choice, index)
         console.log('click')
+            if (choice === quizQuestion[i].answer){
+                console.log("You win!")
+            } else {
+                console.log("You lose!")
+            }
     })
-});
+}); i++
+}
 
+$("#play-game").on("click", displayQuestion)
 
 // $(".btn-lrg").on("click", function(){
 //     displayQuestion()
