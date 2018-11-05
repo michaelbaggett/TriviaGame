0fa57c27a0723bcf9c29e69464f4a579c8a236c1
mbag24 <40617051+michaelbaggett@users.noreply.github.com>
2018-11-04 18:36:34 -0600

    removed commented out code that wasn't being used, cleaned up fortmatting

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,10 @@
 //pseudo code for trivia game
 //need array of multiple questions/answers
-// var quizQuestion = [    
-//     {
-//     text: "question text",
-//     choices: ["top", "bottom", "flex"],
-//     answer: "top"
-// },
-//     {
-//     text: "question text",
-//     choices: ["wrong", "correct", "close but no cigar"],
-//     answer: "correct"
-// },
 
-//     {
-//     text: "question text",
-//     choices: ["choice1", "chocie2", "choice3"],
-//     answer: "choice3"
-// }
-// ]
 var correct = 0;
 var incorrect = 0;
 var questionIndex = 0;
-var time = 30;
+var time = 10;
 var intervalID;
 
 
@@ -47,6 +30,40 @@ var quizQuestions2 = [{
             correct: true
         }
     ]
+}, {
+    question: "abbva?",
+    answers: [{
+            text: "12345",
+            correct: false
+        },
+        {
+            text: "67890",
+            correct: true
+        }
+    ]
+}, {
+    question: "Who let the dogs out?",
+    answers: [{
+            text: "Who",
+            correct: true
+        },
+        {
+            text: "jeff",
+            correct: false
+        }
+    ]
+
+}, {
+    question: "what is the best squat",
+    answers: [{
+        text: "overhead",
+        correct: false
+    },
+    {
+        text: "back",
+        correct: true
+    }]
+
 }];
 
 function timer() {
@@ -58,7 +75,8 @@ function countdown() {
     $("#time-display").text("Time left: " + time);
     if (time === 0) {
         stop();
-        console.log("Time's up!");
+        $("#time-display").text("Time's up!");
+        //displayQuestion(quizQuestions2[questionIndex++])
     }
 };
 
@@ -68,11 +86,9 @@ function stop() {
 
 function reset() {
     clearInterval(intervalID);
-    time = 30;
+    time = 10;
     timer();
 };
-
-//timer();
 
 var displayQuestion = function (questionObj) {
     $("#question").text(questionObj.question);
@@ -95,7 +111,7 @@ $("#play-game").on("click", function () {
     $(this).hide();
     displayQuestion(quizQuestions2[questionIndex++]);
     timer();
-})
+});
 
 $("#test").on("click", ".answer", function () {
 
@@ -127,87 +143,3 @@ $("#test").on("click", ".answer", function () {
         }
     }
 });
-
-
-
-
-
-
-
-
-//Erik's comments to try and point me in right direction
-// <div id="questionHolder">
-//      <p>What is the velocity of an unladen swallow?</p>
-// <div>
-//     <button class="answer" data-correct="false">I don't know</button>
-//     <button class="answer" data-correct="true">African or European?</button>
-// </div>
-// </div>
-
-
-
-
-// $("#questionHolder").on("click", ".answer", function(){
-//     //check and see if answer is correct
-//     if ($(this).attr("data-correct") === "true" ){
-//         console.log("correct!")
-//     } else {
-//         console.log("wrong!")
-//     }
-// });
-
-//i = 0
-
-
-
-// THIS WAS MY ORIGINAL ATTEMPT AT CODING THIS ASSIGNMENT. ABOVE IS A NEW POSSIBLE FRAME WORK AFTER TALKING W ERIK ON FRIDAY 11/2
-// var displayQuestion = function(){
-// $("#question").text(quizQuestion[i].text)
-//     // forEach is the fucking worst
-// quizQuestion[i].choices.forEach((choice, index) => {
-//     console.log(choice, index)
-
-//     var choiceClass = choice + index.toString()
-//     $("#choices").append("<li class='" + choiceClass + "' value=" + choice + ">" + choice + "</li>")
-
-//     $(`.${choiceClass}`).on('click', function(event) {
-//         console.log(choice, index)
-//         console.log('click')
-//             if (choice === quizQuestion[i].answer){
-//                 console.log("You win!")
-//             } else {
-//                 console.log("You lose!")
-//             }
-//     })
-// }); i++
-// }
-
-//$("#play-game").on("click", displayQuestion)
-
-// $(".btn-lrg").on("click", function(){
-//     displayQuestion()
-
-// })
-
-
-
-
-
-
-
-//window shows one question at a time
-//user has a set amount of time to answer the question
-
-//only one answer may be selection
-
-//if the player runs out of time before answering, display that time is up and display correct answer
-
-//if the user guesses incorrectly, show correct answer
-
-//if answer correct, say nice job, and display a new question
-
-//have a timeout inbetween new question loading
-
-//after all questions answered:
-//on final screen show correct/incorrect amt of answers
-//option to restart the game
